c17f2ef22e225009f9f8f9a5a0cbc3b928bb2931
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-07-03 19:18:18 +0200

    "copia hecha"

--- models/Dev.model.js
+++ models/Dev.model.js
@@ -1,18 +1,21 @@
+
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
+const devSchema = new Schema(
   {
   username: {type: String, unique: true},
   password: {type: String, required: [true, "Password is required"]},
   email: {type: String, match: [ /^\S+@\S+\.\S+$/ , "Please input a valid email"], lowercase: true},
-  playedGames: [{ type: Schema.Types.ObjectId, ref: "Game", default: []}]
+  playedGames: [{ type: Schema.Types.ObjectId, ref: "Game", default: []}],
+  createdGames: [{ type: Schema.Types.ObjectId, ref: "Game", default: []}]
   }
 );
 
-const User = model("User", userSchema);
+const Dev = model("Dev", devSchema);
 
 
+// Dev model{username, password, email, [played games], [created games]}
 
 
-module.exports = User;
+module.exports = Dev;
--- models/Game.model.js
+++ models/Game.model.js
@@ -12,7 +12,6 @@ const userSchema = new Schema(
 
 const User = model("User", userSchema);
 
+// Game model{name, genre, ???}
 
-
-
-module.exports = User;
+module.exports = Game;
